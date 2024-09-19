bf5c0749d2831456c0e472f33b1c508871ff6532
Dor1ng <3249491721@qq.com>
2024-09-19 22:10:05 +0800

    Add files via upload

---
+++ AftClass_ass.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main()
+{
+    int nb1,nb2;
+    printf("输入一个正整数");
+    scanf("%d",&nb1);
+    for (;;)
+    {
+        if(nb1 <= 0)
+        {
+            printf(" 请重新输入");
+            scanf("%d",&nb1);
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("输入第二个整数");
+    scanf("%d",&nb2);
+    int r = nb1 % nb2;
+    while (1 > 0)
+    {
+        if (r == 0)
+        {
+            if (nb1 >= nb2)
+            {
+                printf("两者的最大公约数为%d",nb2);
+                return 0;
+            }
+            else{
+                printf("两者的最大公约数为%d",nb1);
+                return 0;
+            }
+        }
+        else
+        {
+            nb1 = nb2;
+            nb2 = r;
+            r = nb1 % nb2;
+        }
+
+    }
+}
